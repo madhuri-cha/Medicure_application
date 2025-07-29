ca6799a15fd33ba4d7702e354316c3f5ef4c9b8e
madhuri-cha <madhurichavan15122003@gmail.com>
2025-07-29 19:41:30 +0530

    admin folder is added

---
+++ admin/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+
+import Login from './pages/Login'
+
+function App() {
+ 
+  return (
+    <>
+     <div>
+       <Login />
+       heyy
+     </div>
+    </>
+  )
+}
+
+export default App
